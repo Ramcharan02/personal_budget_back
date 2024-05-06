e3446983eceaec4dc55a877cfc5a40e026dbbda9
Ramcharan02 <ramcharanrc02@gmail.com>
2024-05-06 14:57:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = process.env.PORT || 3000; 
-const host = 0.0.0.0;
+const host = "localhost";
 
 const cors = require("cors");
 
